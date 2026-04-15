de7bc5b4eff7ad7aa618ec76735d4ccc5adcee6e
Abhibita Sanya <abhibitasanya@gmail.com>
2026-04-15 15:37:42 +0530

    Update CORS for Render frontend

--- server/app.py
+++ server/app.py
@@ -17,6 +17,7 @@ CORS(app, resources={r"/api/*": {"origins": [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://wavelet-music.netlify.app"
+    "https://wavelet-6qht.onrender.com"
 ]}})
 
 
